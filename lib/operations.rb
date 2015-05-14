2375864b2e299490277eb4faef0ace660f4df397
Linus Liang <linus.liang@nyu.edu>
2015-05-14 08:53:00 -0400

    added methods to operations.rb

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,29 @@
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		return true
+	end
+	return false
 end
 
 
 
 def not_safe?(speed)
-
+	return speed > 60 || speed < 40 ? true : false
 end
 
 
 
 def sum_machine(first, second)
-
+	ans = 0 
+	if [first, second].max == second
+		for i in (first..second)
+			ans += i
+		end
+	else
+		for i in (second..first)
+			ans += i
+		end
+	end
+	return ans
 end
+
