4eeb995b2ec077941b90a5821e3172f4222fc004
Omrika <omrikafolkes@gmail.com>
2015-05-27 21:20:40 -0400

    Done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,33 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    true
+  else speed == 40..60
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-
+  speed > 60 || speed < 40 ? true : false 
 end
 
 
 
 def sum_machine(first, second)
-
+  sum = 0
+  if first < second
+    while first < second
+     sum += first
+     first += 1
+    end
+    sum += first
+   else
+      while second < first
+        sum += second
+        second += 1
+      end
+      sum += second
+  end
 end
+ 
