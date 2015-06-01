fdc105425f316c3b014e68172808480d2a79d545
matbalez <matbalez@gmail.com>
2015-06-01 03:11:35 -0400

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,25 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-
+  (speed > 60) || (speed < 40) ? true : false  
 end
 
 
 
 def sum_machine(first, second)
-
+  result = 0
+  current = [first, second].min
+  while current <= [first, second].max
+    result += current
+    current += 1
+  end
+  result
 end
