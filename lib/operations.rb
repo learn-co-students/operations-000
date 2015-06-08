93523c947b95ea5f526bb6e301757f4799582b01
aeremin <aeremin@maxus.ru>
2015-06-08 16:57:50 +0300

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,26 @@
 def unsafe?(speed)
-
+  !(40..60).cover?(speed)
 end
 
 
 
 def not_safe?(speed)
-
+  speed < 40 ? true : (speed > 60 ? true : false)
 end
 
 
 
 def sum_machine(first, second)
+  if first > second then
+    tmp = second
+	second = first
+	first = tmp
+  end
 
+  sum = 0
+  for counter in first..second do
+    sum += counter
+  end
+  
+  sum
 end
